74a72c1cc03b518bfb81cb2f70cedde0bb9664cc
vvc3303 <vvc3303@users.noreply.github.com>
2026-08-03 09:09:29 +0800

    feat: 替换微信二维码为真实二维码图片

--- src/contact.ts
+++ src/contact.ts
@@ -1,6 +1,6 @@
 export const contact = {
   wechatId: '15665470405',
-  wechatQr: '/contact/wechat-qr.svg',
+  wechatQr: '/contact/wechat-qr.jpg',
   qq: '1986582952',
   email: 'zxc0510302022@163.com',
 };
